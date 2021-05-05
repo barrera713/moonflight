c3dd29bec8a9523564bc30d89e5215d156ad58bc
carlos barrera <dev.carlos96@gmail.com>
2021-05-04 21:31:09 -0500

    added logic to parse price

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const fetch = require("node-fetch");
 
-const testing = async () => {
+const Main = () => {
 
-    try {
-        const getData = await fetch("https://sochain.com//api/v2/get_price/DOGE/USD");
-        const json = await getData.json();
-        console.log(json.data.prices[0].price)
-    } catch (err) {
-        console.log(err)
+
+    const fetchDogePrice = async () => {
+
+        let dogePrice = Number;
+
+        try {
+            const getData = await fetch("https://sochain.com//api/v2/get_price/DOGE/USD");
+            /*
+                API returns price from two indices
+                randomly one of these returns 0.0 
+                while the other returns the actual price
+            */
+
+            const dogeObject = await getData.json();
+
+            console.log(dogeObject.data.prices[0].price)
+
+            if(parseFloat(dogeObject.data.prices[0].price) !== 0.0) {
+                dogePrice = dogeObject.data.prices[0].price
+            } else {
+                dogePrice = dogeObject.data.prices[1].price;
+            };
+            
+            console.log("[PRICE] => ", parseFloat(dogePrice));
+
+        } catch (err) {
+            console.log("[ERROR]", err)
+        }
     }
+
+    setInterval(fetchDogePrice, 60000); // one minute
 };
 
-testing();
+Main();
