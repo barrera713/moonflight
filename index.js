d417522edd5e7d378d99686323e4fb5ffcaca3a4
carlos barrera <dev.carlos96@gmail.com>
2021-05-04 22:10:49 -0500

    added twilio

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const fetch = require("node-fetch");
+const accoundSid = process.env.SID;
+const AT = process.env.AT;
+const client = require("twilio")(accoundSid, AT);
+
 
 const Main = () => {
 
+    let dogePrice = Number;
 
     const fetchDogePrice = async () => {
 
-        let dogePrice = Number;
 
         try {
             const getData = await fetch("https://sochain.com//api/v2/get_price/DOGE/USD");
             /*
+                EJECT PRICE from response
+
                 API returns price from two indices
                 randomly one of these returns 0.0 
                 while the other returns the actual price
@@ -17,22 +23,47 @@ const Main = () => {
 
             const dogeObject = await getData.json();
 
-            console.log(dogeObject.data.prices[0].price)
-
             if(parseFloat(dogeObject.data.prices[0].price) !== 0.0) {
                 dogePrice = dogeObject.data.prices[0].price
             } else {
                 dogePrice = dogeObject.data.prices[1].price;
             };
             
-            console.log("[PRICE] => ", parseFloat(dogePrice));
-
         } catch (err) {
             console.log("[ERROR]", err)
         }
     }
 
     setInterval(fetchDogePrice, 60000); // one minute
+
+
+    const sendSMSForHighPrice = async () => {
+        await client.messages
+        .create({
+            body: `***HIGH price has been met! The current Dogecoin price is at ${dogePrice}`,
+            from: '+14053745065',
+            to: '+17133736474'
+        });
+    };
+
+    const sendSMSForDropPrice = async () => {
+        await client.messages
+        .create({
+            body: `###LOW price has been met! The current Dogecoin price is at ${dogePrice}`,
+            from: '+14053745065',
+            to: '+17133736474'
+        });
+    };
+
+
+    if(parseFloat(dogePrice) > 0.5) {
+        sendSMSForHighPrice();
+    } else if('') {
+        sendSMSForDropPrice();
+    } else {
+        console.log("climbing or dropping...")
+    }
+
 };
 
 Main();
