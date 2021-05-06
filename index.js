ad7276a4db212378ee10cae290333d7e5583c195
carlos barrera <dev.carlos96@gmail.com>
2021-05-05 20:47:28 -0500

    added fetch logic for Ethereum

--- index.js
+++ index.js
@@ -7,75 +7,79 @@ const client = require("twilio")(accoundSid, AT);
 
 const Main = () => {
 
-    let dogePrice;
-
-    const fetchDogePrice = async () => {
-
-
+    let etherPriceUSD;
+    const fetchEtherPrice = async () => {
         try {
-            const getData = await fetch("https://sochain.com//api/v2/get_price/DOGE/USD");
-            /*
-                EJECT PRICE from response
-
-                API returns price from two indices.
-                Randomly one of these returns 0.0 
-                while the other returns the actual price
-            */
-
-            const dogeObject = await getData.json();
-            if(parseFloat(dogeObject.data.prices[0].price) !== 0.0) {
-                dogePrice = dogeObject.data.prices[0].price
-            } else {
-                dogePrice = dogeObject.data.prices[1].price;
-            };
-
-
-            if(parseFloat(dogePrice) > 0.55) {
-                sendSMSForHighPrice();
-            } else if(parseFloat(dogePrice) < 0.55) {
-                sendSMSForDropPrice();
-            } else {
-                console.log("climbing or dropping...")
-            }
-            
+            const response = await fetch(`https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${process.env.ES}`)
+            const etherData = await response.json();
+            etherPriceUSD = etherData.result.ethusd;
+            console.log(etherPriceUSD)
         } catch (err) {
-            console.log("[ERROR]", err)
+            console.log("[Ether ERROR]", err)
         }
     }
-    
-    // setInterval(fetchDogePrice); // one minute
-    fetchDogePrice();
+    fetchEtherPrice();
+
+    // let dogePrice;
+
+    // const fetchDogePrice = async () => {
+    //     try {
+    //         const getData = await fetch("https://sochain.com//api/v2/get_price/DOGE/USD");
+    //         /*
+    //             EJECT PRICE from response
+
+    //             API returns price from two indices.
+    //             Randomly one of these returns 0.0 
+    //             while the other returns the actual price
+    //         */
+    //         const dogeObject = await getData.json();
+    //         if(parseFloat(dogeObject.data.prices[0].price) !== 0.0) {
+    //             dogePrice = dogeObject.data.prices[0].price
+    //         } else {
+    //             dogePrice = dogeObject.data.prices[1].price;
+    //         };
 
 
+    //         if(parseFloat(dogePrice) > 0.55) {
+    //             sendSMSForHighPrice();
+    //         } else if(parseFloat(dogePrice) < 0.55) {
+    //             sendSMSForDropPrice();
+    //         } else {
+    //             console.log("climbing...")
+    //         };
+            
+    //     } catch (err) {
+    //         console.log("[ERROR]", err)
+    //     }
+    // }
 
-    const sendSMSForHighPrice = async () => {
+    // // setInterval(fetchDogePrice); // one minute
+    // fetchDogePrice();
+    // const sendSMSForHighPrice = async () => {
+    //     try {
+    //         await client.messages
+    //         .create({
+    //             body: `***HIGH*** price has been met! The current Dogecoin price is at ${dogePrice}`,
+    //             from: process.env.clientNum,
+    //             to: process.env.myNum
+    //         });
+    //     } catch (err) {
+    //         console.log("[SMS High Error]", err)
+    //     };
+    // };
 
-        try {
-            await client.messages
-            .create({
-                body: `***HIGH price has been met! The current Dogecoin price is at ${dogePrice}`,
-                from: '+14053745065',
-                to: '+17133736474'
-            });
-        } catch (err) {
-            console.log("[SMS High Error]", err)
-        }
-    };
-
-    const sendSMSForDropPrice = async () => {
-
-        try {
-            await client.messages
-            .create({
-                body: `###LOW price has been met! The current Dogecoin price is at ${dogePrice}`,
-                from: '+14053745065',
-                to: '+17133736474'
-            });
-        } catch (err) {
-            console.log("[SMS Drop Error]", err)
-        }
-    };
-
+    // const sendSMSForDropPrice = async () => {
+    //     try {
+    //         await client.messages
+    //         .create({
+    //             body: `###LOW### price has been met! The current Dogecoin price is at ${dogePrice}`,
+    //             from: process.env.clientNum,
+    //             to: process.env.myNum
+    //         });
+    //     } catch (err) {
+    //         console.log("[SMS Drop Error]", err)
+    //     }
+    // };
 };
 
 Main();
